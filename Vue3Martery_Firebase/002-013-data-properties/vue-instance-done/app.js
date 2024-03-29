ff8854ed1b29e9ee38a70da327ec5c5fe7c56894
Diogo Leite <diogoleitefaria@gmail.com>
2024-03-29 22:29:53 +0000

    restart... 2-checkopoint

---
+++ Vue3Martery_Firebase/002-013-data-properties/vue-instance-done/app.js
@@ -0,0 +1,15 @@
+const definition = {
+  data() {
+    return {
+      name: 'John'
+    }
+  },
+  
+};
+
+
+const app = Vue.createApp(definition);
+const app2 = Vue.createApp(definition);
+
+app.mount('#app');
+app2.mount('#app2');
