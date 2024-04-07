994419e193304365c4ddd77b27eabb678333dabf
Diogo Leite <diogoleitefaria@gmail.com>
2024-04-07 21:01:42 +0100

    053 sfc covert old app

--- Vue3Martery_Firebase/004-sfc/sfc/src/App.vue
+++ Vue3Martery_Firebase/004-sfc/sfc/src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="p-8 w-3/4 mx-auto">
+    <div class="mb-4">
+      <add-task-input @added="taskAdded"></add-task-input>
+      <base-checkbox
+        class="mb-4 p-4 text-gray-600 text-sm font-weight-100"
+        v-model="onlyPending"
+      >
+        Only pending tasks
+      </base-checkbox>
+    </div>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+      <todo-list-item
+        v-for="task in displayedTasks"
+        :task="task"
+        :key="task.id"
+        v-model:done="task.done"
+        v-model:priority="task.priority"
+      >
+      </todo-list-item>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AddTaskInput from "./components/AddTaskInput.vue";
+import BaseCheckbox from "./components/BaseCheckbox.vue";
+import TodoListItem from "./components/TodoListItem.vue";
 
+let nextTaskId = 100;
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { AddTaskInput, BaseCheckbox, TodoListItem },
+  data() {
+    return {
+      onlyPending: false,
+      tasks: [
+        {
+          id: 1,
+          description: "Buy food for the dog",
+          priority: false,
+          done: false,
+        },
+        {
+          id: 2,
+          description: "Pay the bills",
+          priority: true,
+          done: false,
+        },
+        {
+          id: 3,
+          description: "Buy some computer games",
+          priority: false,
+          done: true,
+        },
+        {
+          id: 4,
+          description: "Go to the gym",
+          priority: false,
+          done: false,
+        },
+      ],
+    };
+  },
+  computed: {
+    displayedTasks() {
+      return [...this.tasks]
+        .sort((a, b) => Number(b.priority) - Number(a.priority))
+        .filter((task) => !this.onlyPending || !task.done);
+    },
+  },
+  methods: {
+    taskAdded(task) {
+      this.tasks.push({
+        id: nextTaskId++,
+        description: task,
+        done: false,
+        priority: false,
+      });
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ Vue3Martery_Firebase/004-sfc/sfc/src/components/TodoListItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4"
+    :class="{ 'opacity-25 line-through': task.done }"
+  >
+    <div>
+      {{ task.description }}
+    </div>
+    <div class="py-4 bg-white">
+      <base-checkbox
+        class="mb-2"
+        @update:model-value="$emit('update:done', $event)"
+        :model-value="done"
+      >
+        Done
+      </base-checkbox>
+      <base-checkbox
+        @update:model-value="$emit('update:priority', $event)"
+        :model-value="priority"
+      >
+        Prioritized
+      </base-checkbox>
+    </div>
+  </div>
+</template>
+<script>
+import BaseCheckbox from "./BaseCheckbox.vue";
+export default {
+  components: {
+    BaseCheckbox,
+  },
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+    done: {
+      type: Boolean,
+    },
+    priority: {
+      type: Boolean,
+    },
+  },
+  emits: ["update:done", "update:priority"],
+};
+</script>
